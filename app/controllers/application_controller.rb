91b9796324df2897072acf9e8b79dbae7e56abf9
OgembaJr <emmanuel.ogemba@student.moringaschool.com>
2023-03-28 16:33:29 +0300

    Completed password lab

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,10 @@
 class ApplicationController < ActionController::API
   include ActionController::Cookies
+  helper_method :current_user
 
+  private
+
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
 end
